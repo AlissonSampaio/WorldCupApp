a9ec903b2639120f8ad9649b8c614bbf04e44d79
Alisson Alves <alisson2012alves@gmail.com>
2022-11-23 23:20:52 -0300

    Initial Commit

---
+++ World Cup/World_CupApp.swift
@@ -0,0 +1,17 @@
+//
+//  World_CupApp.swift
+//  World Cup
+//
+//  Created by Alisson Alves on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct World_CupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
